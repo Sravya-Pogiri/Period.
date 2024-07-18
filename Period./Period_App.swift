38b794f51fbd0a4a27454efcb2bd4eaf85407828
Sravya Pogiri <sravya.pogiri@gmail.com>
2024-07-18 17:08:39 -0400

    Initial Commit

---
+++ Period./Period_App.swift
@@ -0,0 +1,17 @@
+//
+//  Period_App.swift
+//  Period.
+//
+//  Created by Sravya on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Period_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
