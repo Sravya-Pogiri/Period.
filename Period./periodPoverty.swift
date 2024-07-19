fab04ab4ecd6c797a7f93fd63467d0d51f46ac43
Paige Randell <fulalove2@gmail.com>
2024-07-19 12:26:35 -0500

    period poverty

---
+++ Period./periodPoverty.swift
@@ -0,0 +1,18 @@
+//
+//  periodPoverty.swift
+//  Period.
+//
+//  Created by paige randell on 7/19/24.
+//
+
+import SwiftUI
+
+struct periodPoverty: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    periodPoverty()
+}
